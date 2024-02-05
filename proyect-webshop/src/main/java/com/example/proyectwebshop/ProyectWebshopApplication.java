8fb506540c1bbfe7a94236561e996015504dc682
CarolinaVelandia <carolinavelandiacelis@gmail.com>
2024-02-05 17:43:49 -0500

    agregue springboot al proyecto

---
+++ proyect-webshop/src/main/java/com/example/proyectwebshop/ProyectWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectWebshopApplication.class, args);
+	}
+
+}
---
+++ proyect-webshop/src/test/java/com/example/proyectwebshop/ProyectWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
